77c6813b703de0607d90f893f42269004d3fbfa5
Vladimir <uipertest@gmail.com>
2026-06-18 17:29:23 +0700

    Add fallbacks for SECRET_KEY and ALGORITHM in securety.py

--- Backend/app/core/securety.py
+++ Backend/app/core/securety.py
@@ -13,8 +13,8 @@ from jose import JWTError
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
+SECRET_KEY = os.getenv("SECRET_KEY", "VladimirLesnik")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "300"))
 REFRESH_TOKEN_EXPIRE_MINUTES = int(os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES", "60"))
 
